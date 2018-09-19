dac1ff4c29229990e9d04b87d1c93d351b6cfdb8
Marcus Tenorio <martxyz@gmail.com>
2018-09-19 19:52:55 -0300

    First commit.

---
+++ PLP-C/criptoforca.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+
+int main() {
+
+
+
+
+switch (desenho) {
+				case 0:
+					printf("\n\n");
+					printf("  ##########***##  \n");
+					printf("  ####            \n");
+					printf("  ####            \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					break;
+				case 1:
+					printf("\n\n");
+					printf("  ##########***##  \n");
+					printf("  ####       *    \n");
+					printf("  ####      ***   \n");
+					printf("  ##       *o o*  \n");
+					printf("  ##       * ^ *  \n");
+					printf("  ##         *    \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					break;
+				case 2:
+					printf("\n\n");
+					printf("  ##########***##  \n");
+					printf("  ####       *    \n");
+					printf("  ####      ***   \n");
+					printf("  ##       *o o*  \n");
+					printf("  ##       * ^ *  \n");
+					printf("  ##         *    \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					break;
+				case 3:
+					printf("\n\n");
+					printf("  ##########***##  \n");
+					printf("  ####       *    \n");
+					printf("  ####      ***   \n");
+					printf("  ##       *o o*  \n");
+					printf("  ##       * ^ *  \n");
+					printf("  ##         *    \n");
+					printf("  ##         |    \n");
+					printf("  ##        /|    \n");
+					printf("  ##       / |    \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					break;
+				case 4:
+					printf("\n\n");
+					printf("  ##########***##  \n");
+					printf("  ####       *    \n");
+					printf("  ####      ***   \n");
+					printf("  ##       *o o*  \n");
+					printf("  ##       * ^ *  \n");
+					printf("  ##         *    \n");
+					printf("  ##         |    \n");
+					printf("  ##        /|\\  \n");
+					printf("  ##       / | \\ \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##              \n");
+					printf("  ##              \n");
+					break;
+				case 5:
+					printf("\n\n");
+					printf("  ##########***##  \n");
+					printf("  ####       *    \n");
+					printf("  ####      ***   \n");
+					printf("  ##       *o o*  \n");
+					printf("  ##       * ^ *  \n");
+					printf("  ##         *    \n");
+					printf("  ##         |    \n");
+					printf("  ##        /|\\  \n");
+					printf("  ##       / | \\ \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##        /     \n");
+					printf("  ##       /      \n");
+					break;
+				case 6:
+					printf("\n\n");
+					printf("  ##########***##  \n");
+					printf("  ####       *    \n");
+					printf("  ####      ***   \n");
+					printf("  ##       *X X*  \n");
+					printf("  ##       * ^ *  \n");
+					printf("  ##         *    \n");
+					printf("  ##         |    \n");
+					printf("  ##        /|\\   \n");
+					printf("  ##       / | \\  \n");
+					printf("  ##         |    \n");
+					printf("  ##         |    \n");
+					printf("  ##        / \\   \n");
+					printf("  ##       /   \\  \n");
+					break;
+				}
