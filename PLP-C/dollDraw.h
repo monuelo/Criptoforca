f0f85b674be53496ba423a34b3b13d48da606888
Laerson Saraiva <laerson.verissimo@ccc.ufcg.edu.br>
2018-09-24 09:24:13 -0300

    Update dollDraw.h

--- PLP-C/dollDraw.h
+++ PLP-C/dollDraw.h
@@ -1,16 +1,5 @@
 /*dollDraw.h -- constantes e declarações para dollDraw.c*/
 
 
-//desenhos dos vários estágios da forca, de acordo com as vidas restantes
-void printDoll_5_lives(void);
-void printDoll_4_lives(void);
-void printDoll_3_lives(void);
-void printDoll_2_lives(void);
-void printDoll_1_lives(void);
-void printDoll_0_lives(void);
-
-//desenho do final do jogo (derrota)
-void printDoll_dead(void);
-
 //chamada da função adequada para desenhar na tela
 void printDoll_state(int nextState);
