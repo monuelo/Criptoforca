ba8d6fa1372a9059ef9714d5e6baaa9e781a9c24
Laerson Saraiva <laerson.verissimo@ccc.ufcg.edu.br>
2018-09-24 07:57:45 -0300

    Create cripto_functions.h

---
+++ PLP-C/cripto_functions.h
@@ -0,0 +1,10 @@
+/*cripto_functions.h -- constantes e declarações para criptoFunctions.c*/
+
+//Definição das constantes
+//ex: #define CESAR1 0
+
+
+void cipherStrategy(int strategy, char *uncrypted_word);
+int fibonacciSequence(int n);
+
+char* shift(char *palavra);
