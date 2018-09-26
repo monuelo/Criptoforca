49f979ce0a84656bb4eb1ec8c8f605653be79415
Vitor Diniz <39504013+dinizvitorr@users.noreply.github.com>
2018-09-26 00:03:58 -0300

    Update cripto-functions.c

--- PLP-C/cripto-functions.c
+++ PLP-C/cripto-functions.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include "cripto_functions.h"
 
 static int fibonacciSequence(int n);
@@ -70,11 +71,78 @@ char* fibonacciCripto(char* palavra) {
 	}
 	return word;
 }
+/**
+ * complementarAscii: substitui cada caractere pelo seu complementar da tabela AscII. Exemplo: AscII de 'A' é 65,
+ * logo, seu complementar é 127-65 = 62.
+ * PS.: Os caracteres de códigos complementares entre 0 e 31 sao deslocados 31 caracteres para frente
+ */
+char* complementarAscii (char* palavra){
+	char *word = malloc(sizeof(char) * strlen (palavra));
+	for (int i = 0; i < strlen (palavra); i++){
+		int codeAsc = ( 127 - (int)palavra[i]);
+		if (codeAsc < 31)
+			codeAsc += 31;	
+		word[i] = (char)codeAsc;
+	}
+	return word;
+}
+
+/**
+ * checkIndex:  auxiliar de shuffle 
+ */
+int checkIndex (int j, int* indices, int size){
+	for (int i = 0; i < size; i++)
+		if (j == indices[i]){	// se o indice j ja foi utilizado, test = 1
+			return 1;
+		}
+	return 0;
+}
+/**
+ * shuffle:  criptografia para embaralhar as letras da string
+ */
+char* shuffle(char* palavra) {
+	srand(time(NULL));
+	char *word = malloc(sizeof(char) * strlen(palavra));
+	int *indices = malloc(sizeof(int)* strlen(palavra));
+
+	for (int i = 0; i < strlen(palavra); i++)
+		indices[i] = -1;
+
+	for (int i = 0; i < strlen(palavra); i++){
+		int j = rand() % strlen(palavra);
+		int test = checkIndex(j, indices, strlen(palavra));
+
+		if (test == 0){
+			word[j] = palavra[i];
+			indices[i] = j;
+		}
+		else
+			i--;
+	}
+	return word;
+}
+
+/**
+ * criptoMix: realiza 4 criptografias em sequencia para a mesma palavra
+ */
+char* criptoMix (char* palavra){
+	char *word = malloc(sizeof(char) * strlen(palavra));
+	word = shuffle (palavra);
+	word = cesar1 (word);
+	word = complementarAscii(word);
+	word = fibonacciCripto(word);
+
+	return word;
+}
+
 
 /*int main() {
 	char oi[30] = "pipoca";
 	printf("shift: %s\n", shift(oi));
 	printf("cesar1: %s\n", cesar1(oi));
 	printf("fibonacci: %s\n", fibonacciCripto(oi));
+	printf("shuffle: %s\n", shuffle(oi));
+	printf("complementarAscii: %s\n", complementarAscii(oi));
+	printf("Criptomix: %s\n", criptomix(oi));
 	return 0;
 }*/ 
