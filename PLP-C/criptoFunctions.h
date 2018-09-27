85251b9675a0552c23408704675c3ea823c4fa1b
Laerson Saraiva <laerson.verissimo@ccc.ufcg.edu.br>
2018-09-27 13:18:50 -0300

    Update criptoFunctions.h

--- PLP-C/criptoFunctions.h
+++ PLP-C/criptoFunctions.h
@@ -1,13 +1,18 @@
-/*criptoFunctions.h -- constantes e declarações para criptoFunctions.c*/
+/*cripto_functions.h -- constantes e declarações para criptoFunctions.c*/
 
 //Definição das constantes
-//ex: #define CESAR1 0
+/*ESSES SÃO OS VALORES A SEREM PASSADOS AO CHAMAR A FUNÇÃO CIPHERSTRATEGY(). CADA UM EXECUTA UMA ESTRATÉGIA DE CIFRAGEM DIFERENTE*/
+#define CESAR 0
+#define SHIFT 1
+#define FIBONACCI 2
+#define RSA 3
+#define COMPLEMENTAR_ASCII 4
+#define SHUFFLE 5
+#define CRIPTOMIX 6
+
+
 
 
 void cipherStrategy(int strategy, char *uncrypted_word);
 
 
-char* shift(char *palavra); //cifra de troca(?)
-char* cesar1(char* palavra); //cifra de cesar
-char* fibonacciCripto(char* palavra); //cifra que usa sequencia de Fibonnacci 
-char* rsa(char* palavra);
