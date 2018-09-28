9dec5a49b0f19bfb862e1094b11a3dcdc059e1b1
Laerson Saraiva <laerson.verissimo@ccc.ufcg.edu.br>
2018-09-28 03:26:36 -0300

    Update RSA.h

--- PLP-C/RSA.h
+++ PLP-C/RSA.h
@@ -1,9 +1,9 @@
-#define MAX_PRIME 13 //maior número primo usado pra gerar as chaves
-#define TOTIENT(X, Y) ((X-1)*(Y-1))
+#define MAX_PRIME 25 //maior número primo usado pra gerar as chaves
+
 
 void keyGen(struct public_key_class *pub, struct private_key_class *priv); //Gera as chaves
 long long *rsa_encrypt(const char *message, const unsigned long message_size, const struct public_key_class *public); //cifra a mensagem
-char *rsa_decrypt(const long long *message, const unsigned long message_size, const struct private_key_class *pub); //descifra a mensagem (pra testes)
+//char *rsa_decrypt(const long long *message, const unsigned long message_size, const struct private_key_class *pub); //descifra a mensagem (pra testes)
 
 struct public_key_class {
 	long long modulus;
@@ -14,5 +14,3 @@ struct private_key_class {
 	long long modulus;
 	long long exponent;
 };
-
-
