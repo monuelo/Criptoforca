5af0b6a3b1e4cc6f22a2fd193023972e400a4f28
Marcus Tenorio <martxyz@gmail.com>
2018-10-20 14:12:50 -0300

    Trying to avoid the zero :P

--- PLP-HASKELL/criptoforca.hs
+++ PLP-HASKELL/criptoforca.hs
@@ -12,3 +12,13 @@ Menu = do
     putStrLn "    3- Medio "
     putStrLn "    4- Enigma"
     clearScreen
+
+
+shiftStr :: Int -> String -> String
+shiftStr n = map shiftChar
+  where
+    alphabet = ['a'..'z']
+    alphaLoop = cycle alphabet
+    shiftChar c = head
+        $ drop (length alphabet + n)
+        $ dropWhile (/= c) alphaLoop
