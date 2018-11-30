ebb03892962c7827ef1f0f1b440f6fe4e63b6e4f
Marcus Tenorio <martxyz@gmail.com>
2018-11-29 23:15:42 -0300

    Update palavras.pl

--- PLP-PROLOG/palavras.pl
+++ PLP-PROLOG/palavras.pl
@@ -2,4 +2,17 @@
 
 word(banana, comida, 2).
 word(lhama, animal, 1).
-
+word(barata, animal, 1).
+word(camelo, animal, 1).
+word(rato, animal, 1).
+word(gato, animal, 1).
+word(cachorro, animal, 1).
+word(panda, animal, 1).
+word(pombo, animal, 1).
+word(batata, comida, 2).
+word(pato, animal, 1).
+word(urso, animal, 1).
+word(salamandra, animal, 3).
+word(rinoceronte, animal, 3).
+word(ornitorrinco, animal, 3).
+word(everton,professor,3).
